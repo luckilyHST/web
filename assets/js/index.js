a6c9c9a2695d219252e73b9ece3208297f4f87dc
HST <1007804106@qq.com>
2021-04-28 19:53:48 +0800

    完成了新的功能

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,21 @@
-$(function(){
-    $.ajax({
-        method: 'GET',
-        url: '/my/userinfo',
-        success: function(res){
-            if(res.status !== 0){
-                layui.layer.msg(res.message)
+
+    getUserInfo();
+    function getUserInfo(){
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            success: function(res){
+                if(res.status !== 0){
+                    layui.layer.msg(res.message)
+                }
+                // console.log(res)
+                renderAvatar(res.data)
             }
-            console.log(res)
-            getUserInfo(res.data)
-        }
-    })
+        })
+    }
 
     //渲染用户头像
-    function getUserInfo(user){
+    function renderAvatar(user){
         var name = user.nickname || user.username
         $('#greet').html('欢迎&nbsp;&nbsp;' + name)
         if(user.user_pic === null){
@@ -21,7 +24,7 @@ $(function(){
             $('.useravatr').html(text).show
         }else{
             $('.layui-nav-img').attr('src',user.user_pic).show();
-            $('.useravatr').html(text).show()
+            $('.useravatr').html(text).hide()
         }
     }
 
@@ -31,8 +34,11 @@ $(function(){
         layer.confirm('确认退出登录?', {icon: 3, title:'提示'}, function(index){
             //do something
             localStorage.removeItem('token')
-            location.href='http://127.0.0.1:5500/login.html'
+            location.href='http://127.0.0.1:5501/%E5%89%8D%E7%AB%AF/%E5%9F%BA%E7%A1%80/%E9%98%B6%E6%AE%B5%E5%9B%9B%20%E5%89%8D%E5%90%8E%E7%AB%AF%E4%BA%A4%E4%BA%92/web/login.html'
             layer.close(index);
           })
     })
-})
+
+    
+
+
